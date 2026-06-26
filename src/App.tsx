272f833c0d9592f02ad68c90928403517de7e855
NawabIkram <nawabikram786@gmail.com>
2026-06-26 17:51:26 +0500

    feat: wire app routes and language provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+﻿import { Route, Routes } from "react-router-dom";
 import { AppShell } from "./components/AppShell";
 import { AnnouncementsPage } from "./pages/AnnouncementsPage";
 import { CheckoutPage } from "./pages/CheckoutPage";
@@ -7,38 +7,45 @@ import { DemoPage } from "./pages/DemoPage";
 import { DonationsPage } from "./pages/DonationsPage";
 import { DonorPortalPage } from "./pages/DonorPortalPage";
 import { DonorsPage } from "./pages/DonorsPage";
+import { EventsPage } from "./pages/EventsPage";
 import { LoginPage } from "./pages/LoginPage";
 import { MarketingPage } from "./pages/MarketingPage";
 import { PrayerTimesPage } from "./pages/PrayerTimesPage";
 import { PricingPage } from "./pages/PricingPage";
 import { ReportsPage } from "./pages/ReportsPage";
 import { SettingsPage } from "./pages/SettingsPage";
+import { StaffPage } from "./pages/StaffPage";
 import { ZakatPage } from "./pages/ZakatPage";
+import { LanguageProvider } from "./i18n/i18n";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MarketingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/demo" element={<DemoPage />} />
-      <Route path="/checkout/:planId" element={<CheckoutPage />} />
-      <Route element={<AppShell />}>
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/donations" element={<DonationsPage />} />
-        <Route path="/zakat" element={<ZakatPage />} />
-        <Route path="/prayer-times" element={<PrayerTimesPage />} />
-        <Route path="/announcements" element={<AnnouncementsPage />} />
-        <Route path="/reports" element={<ReportsPage />} />
-        <Route path="/donors" element={<DonorsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/donor-portal" element={<DonorPortalPage />} />
-        <Route path="/donate" element={<DonorPortalPage view="donate" />} />
-        <Route path="/my-donations" element={<DonorPortalPage view="donations" />} />
-        <Route path="/receipts" element={<DonorPortalPage view="receipts" />} />
-        <Route path="/recurring" element={<DonorPortalPage view="recurring" />} />
-        <Route path="/profile" element={<DonorPortalPage view="profile" />} />
-      </Route>
-    </Routes>
+    <LanguageProvider>
+      <Routes>
+        <Route path="/" element={<MarketingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/demo" element={<DemoPage />} />
+        <Route path="/checkout/:planId" element={<CheckoutPage />} />
+        <Route element={<AppShell />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/donations" element={<DonationsPage />} />
+          <Route path="/zakat" element={<ZakatPage />} />
+          <Route path="/prayer-times" element={<PrayerTimesPage />} />
+          <Route path="/announcements" element={<AnnouncementsPage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+          <Route path="/donors" element={<DonorsPage />} />
+          <Route path="/staff" element={<StaffPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/donor-portal" element={<DonorPortalPage />} />
+          <Route path="/donate" element={<DonorPortalPage view="donate" />} />
+          <Route path="/my-donations" element={<DonorPortalPage view="donations" />} />
+          <Route path="/receipts" element={<DonorPortalPage view="receipts" />} />
+          <Route path="/recurring" element={<DonorPortalPage view="recurring" />} />
+          <Route path="/profile" element={<DonorPortalPage view="profile" />} />
+        </Route>
+      </Routes>
+    </LanguageProvider>
   );
 }
